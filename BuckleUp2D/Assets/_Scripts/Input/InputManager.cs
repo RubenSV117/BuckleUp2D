0382c32d1a359e3b2a2fb5fb3532e46d37af5405
Ruben Sanchez <SanchezVRuben@gmail.com>
2018-05-27 16:37:19 -0700

    Restarted with heist

---
+++ BuckleUp2D/Assets/_Scripts/Input/InputManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Manages controller input
+/// 
+/// Ruben Sanchez
+/// 5/16/18
+/// </summary>
+
+public class InputManager : MonoBehaviour
+{
+    public Vector2 AimSensitivity;
+    public Vector2 AimDamping;
+
+    [HideInInspector] public Vector3 MoveDirection;
+    [HideInInspector] public Vector3 AimDirection;
+
+    public delegate void Attack();
+    public event Attack OnAttack;
+
+    public delegate void Roll();
+    public event Roll OnRoll;
+
+    public delegate void SlowMo();
+    public event SlowMo OnSlowMo;
+
+    public delegate void Aim();
+    public event Aim OnAim;
+
+    public delegate void WeaponCycle();
+    public event WeaponCycle OnWeaponCycle;
+
+    public delegate void SprintStateChange(bool isSprinting);
+    public event SprintStateChange OnSprintChange;
+
+    void Update()
+    {
+        MoveDirection = new Vector3(Input.GetAxis("HorizontalMove"), 0, Input.GetAxis("VerticalMove"));
+        AimDirection = new Vector3(Input.GetAxis("HorizontalAim"), 0, Input.GetAxis("VerticalAim"));
+
+        //attack
+        if (Input.GetAxis("Attack") != 0)
+            OnAttack.Invoke();
+
+        //aim
+        if (Input.GetAxis("Aim") != 0 && OnAim != null)
+            OnAim.Invoke();
+
+        //roll
+        if (Input.GetButtonDown("Roll") && OnRoll != null)
+            OnRoll.Invoke();
+
+        //slowMo
+        if (Input.GetButtonDown("SlowMo") && OnSlowMo != null)
+            OnSlowMo.Invoke();
+
+        //cycle weapon
+        if (Input.GetButtonDown("CycleWeapon") && OnWeaponCycle != null)
+            OnWeaponCycle.Invoke();
+
+        //sprint state change
+        if (Input.GetButtonDown("Sprint") && OnSprintChange != null)
+            OnSprintChange.Invoke(true);
+
+        if (Input.GetButtonUp("Sprint") && OnSprintChange != null)
+            OnSprintChange.Invoke(false);
+    }
+}
--- BuckleUp2D/Assets/_Scripts/Player/PlayerMovement.cs
+++ BuckleUp2D/Assets/_Scripts/Player/PlayerMovement.cs
@@ -1,162 +1,47 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 /// <summary>
-/// Manages player movement
-/// sends direction and PlayerAnimation 
+/// Controls player movement
 /// 
 /// Ruben Sanchez
-/// 5/10/18
+/// 5/27/28
 /// </summary>
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] protected Transform player;
-    [SerializeField] private float runSpeed = 10f;
-    [SerializeField] protected float walkSpeed = 5f;
-    [SerializeField] protected float turnSpeed = 5f;
-    [SerializeField] private UnityEvent onMove;
+    [SerializeField] private InputManager input;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float turnSpeed;
 
-    [SerializeField] private float speedMultiplier = 2f;
-    [SerializeField] private UnityEvent onSprint;
-
-    [SerializeField] private UnityEvent onIdle;
-
-    //Roll
-    [Header("Roll")]
-    [SerializeField] private float rollSpeed = 20f;
-    [SerializeField] private float rollDuration = 1f;
-    [SerializeField] private float rollCooldown = .5f;
-    [SerializeField] private UnityEvent onRoll;
-
-    private Coroutine rollCoroutine;
-    private float rollCooldownTimer;
-    private bool canRoll;
-
-    protected Rigidbody rigidB;
-
-    private bool canControlMove = true;
-
-    //sprint
-    private bool isSprinting;
-
-    protected PlayerAnimation playerAnim;
-    private InputManager input;
-    private float turnInput; 
+    private Rigidbody rigidB;
+    private Transform mTransform;
 
 
     private void Awake()
     {
         rigidB = GetComponent<Rigidbody>();
-        playerAnim = GetComponent<PlayerAnimation>();
-     
+        mTransform = transform;
     }
 
-    private void Start()
-    {
-        GameManager.Instance.InputManager.OnRoll += Roll;
-        GameManager.Instance.InputManager.OnSprintChange += SetSprint;
-        input = GameManager.Instance.InputManager;
-    }
-
-    private void Update()
+    void Update ()
     {
         Move();
         Turn();
-
-        if (!canRoll)
-        {
-            rollCooldownTimer += Time.deltaTime;
-
-            if (rollCooldownTimer >= rollCooldown)
-            {
-                rollCooldownTimer = 0;
-                canRoll = true;
-            }
-        }
-
-        if(!GroundDetector.isGrounded)
-            rigidB.velocity += Vector3.down * runSpeed;
     }
 
-    public virtual void Turn()
+    private void Move()
     {
-        if (canControlMove)
-        {
-            turnInput = Mathf.Lerp(turnInput, input.AimDirection.x, 1f / input.AimDamping.x);
-            player.Rotate(Vector3.up, turnInput * turnSpeed);
-        }
-            
+        Vector3 moveDirection = input.MoveDirection.z * mTransform.forward;
+        moveDirection += input.MoveDirection.x * mTransform.right;
+        moveDirection = Vector3.Normalize(moveDirection);
+
+        rigidB.velocity = moveDirection * moveSpeed;
     }
 
-    public virtual void Move ()
+    private void Turn()
     {
-        if (canControlMove)
-        {
-            Vector3 direction = Vector3.Normalize(input.MoveDirection);
-
-            Vector3 dirRelativeToPlayer = new Vector3();
-            dirRelativeToPlayer += direction.x * player.right;
-            dirRelativeToPlayer += direction.z * player.forward;
-            dirRelativeToPlayer = Vector3.Normalize(dirRelativeToPlayer);
-
-            float speed = input.MoveDirection.magnitude > .5f ? runSpeed : walkSpeed;
-            Vector3 moveVelocity = isSprinting ? dirRelativeToPlayer * speed * speedMultiplier : dirRelativeToPlayer * speed;
-
-            rigidB.velocity =  new Vector3(moveVelocity.x, rigidB.velocity.y, moveVelocity.z);
-
-            playerAnim.SetSpeed(input.MoveDirection.magnitude);
-        }
-    }
-
-    public void Stop()
-    {
-        rigidB.velocity = Vector3.zero;
-    }
-
-    public void Roll()
-    {
-        if (canRoll)
-        {
-            canControlMove = false;
-
-            Vector3 direction = Vector3.Normalize(input.MoveDirection);
-
-            Vector3 dirRelativeToPlayer = new Vector3();
-            dirRelativeToPlayer += direction.x * player.right;
-            dirRelativeToPlayer += direction.z * player.forward;
-            dirRelativeToPlayer = Vector3.Normalize(dirRelativeToPlayer);
-
-            rigidB.velocity = dirRelativeToPlayer * rollSpeed;
-
-            if (rollCoroutine == null)
-                rollCoroutine = StartCoroutine(RollCo());
-
-            onRoll.Invoke();
-        }
-    }
-
-    //restrict control for duration of roll
-    public IEnumerator RollCo()
-    {
-        yield return new WaitForSeconds(rollDuration);
-        canControlMove = true;
-        rollCoroutine = null;
-        canRoll = false;
-    }
-
-    public void SetSprint(bool sprint)
-    {
-        isSprinting = sprint;
-        playerAnim.SetSprint(sprint);
-
-        if(sprint)
-            onSprint.Invoke();
-    }
-
-    public void SetCanControlMove(bool control)
-    {
-        canControlMove = control;
+        mTransform.Rotate(Vector3.up, input.AimDirection.x * turnSpeed);
     }
 }
